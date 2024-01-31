baeaf9675f675dcaab0e305b6090ef619c590f88
PRAKRITI <136888456+Nature110625@users.noreply.github.com>
2024-01-31 22:39:48 +0530

    ...

--- LetUsC_ch14/Question16.c
+++ LetUsC_ch14/Question16.c
@@ -4,6 +4,7 @@ int ck_r(int p[9][9]);
 int ck_c(int p[9][9]);
 int main(){
     int sq, r, c;
+    //given data
     int game[9][9]={
                     5, 3, 4, 6, 7, 8, 9, 1, 2,
                     6, 7, 2, 1, 9, 5, 3, 4, 8,
@@ -18,46 +19,54 @@ int main(){
     sq=ck_sq(game);
     r=ck_r(game);
     c=ck_c(game);
-    if(sq && r && c)
+    if(sq && r && c)    //all functions return true
         printf("Suduko is solved correctly.");
     else
         printf("Suduko is not solved correctly.");              
 }
+
+//check the 3x3 matrices 
 int ck_sq(int p[9][9]){
     int sum_dg=0, count_sq=0;
-    for(int i=0; i<9; i+=3)
-        for(int j=0; j<9; j+=3){
-            sum_dg=0;
-            for(int m=1; m<10; m++)
-                for(int k=i; k<i+3; k++)
-                    for(int l=j; l<j+3; l++)
-                        if(p[k][l]==m)
-                            sum_dg+=m;
-            if(sum_dg==45)
-                count_sq++;
+    for(int i=0; i<9; i+=3)     //for the row of first element of 3x3 matrix
+        for(int j=0; j<9; j+=3){    //for the column of first element of 3x3 matrix
+            sum_dg=0;               //initialized to zero before checking each 3x3 matrices
+            for(int m=1; m<10; m++) //for comparing each element to digits from 1 to 9
+                for(int k=i; k<i+3; k++)    //for row of 3x3 matrix
+                    for(int l=j; l<j+3; l++)    //for column of 3x3 matrix
+                        if(p[k][l]==m)      //sum is more umique than count
+                            sum_dg+=m;      // add only if value in matrix is digit from 1 to 9
+            if(sum_dg==45)                  // if any digit from 1 to 9 will be missing or repeated sum_dg will have diff value 
+                count_sq++;                 //count the no. of sq having all digits from 1 to 9
         }
-    if(count_sq==9)
+    if(count_sq==9)     //if all nine 3x3 matrices have all digits from 1 to 9
         return 1;
     return 0;
 }
+
+//check row
 int ck_r(int p[9][9]){
     int sum_dg=0, count_r=0;
-    for(int i=0; i<9; i++){
+    //divides 9x9 2-d array into nine 1-d matrix of size 9 row wise
+    for(int i=0; i<9; i++){ //index of row to make 1-d array
         sum_dg=0;
-        for(int k=1; k<10; k++)
-            for(int j=0; j<9; j++)
+        for(int k=1; k<10; k++) //for comparing elements of 1-d array formed to digits from 1 to 9
+            for(int j=0; j<9; j++)  //for index of 1-d array formed
                 if(p[i][j]==k)
                     sum_dg+=k;
-        if(sum_dg==45)
+        if(sum_dg==45)  //count the no. of 1-d array having all digits from 1 to 9
             count_r++;
     }
-    if(count_r==9)
+    if(count_r==9)  // if all the 9 1-d array have all digits from 1 to 9
         return 1;
     return 0;
 }
+
+//check column
 int ck_c(int p[9][9]){
     int sum_dg=0, count_c=0;
-    for(int i=0; i<9; i++){
+    //divides 9x9 2-d array into nine 1-d matrix of size 9 column wise
+    for(int i=0; i<9; i++){ //index of column that will make 1-d array
         sum_dg=0;
         for(int k=1; k<10; k++)
             for(int j=0; j<9; j++)
