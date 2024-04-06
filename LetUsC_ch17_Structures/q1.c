40769fb3da351140f662dc1eb0c29a54bef90b85
PRAKRITI <136888456+Nature110625@users.noreply.github.com>
2024-04-06 19:44:09 +0530

    ...

---
+++ LetUsC_ch17_Structures/q1.c
@@ -0,0 +1,56 @@
+/*Create a structure to specify data on students given below:
+Roll number, Name, Department, Course, Year of joining
+Assume that there are not more than 450 students in the college.
+(1) Write a function to print names of all students who joined in a
+particular year.
+(2) Write a function to print the data of a student whose roll
+number is received by the function.*/
+
+#include <stdio.h>
+struct data {
+        int roll_number;
+        char name[50];
+        char department[100];
+        char course[40];
+        int yoj;
+}; 
+void prints_name(int , struct data *, int);
+void prints_data(int , struct data *, int);
+int main(){
+    struct data students[450];
+
+    int year, roll, no_std;
+    char ans;
+    for(int i=0; i<450; i++){
+        printf("enter student %d roll number, name, department and course (in order): ", i+1);
+        scanf("%d%s%s%s%d", &students[i].roll_number, &students[i].name, &students[i].department, &students[i].course, &students[i].yoj);
+        printf("do you want to enter data (y/n): ");
+        scanf("%c", &ans);
+        if(ans=='n'){
+            no_std=i;
+            break;
+        }
+    }
+
+    printf("enter the year of joining: ");
+    scanf("%d", &year);
+    prints_name(year, &students[0], no_std);
+    
+    printf("enter the roll number: ");
+    scanf("%d", &roll);
+    prints_data(roll, &students[0], no_std);
+}
+
+void prints_name(int year, struct data *std, int n){
+    for(int i=0; i<=n; i++){
+        if(std[i].yoj==year)
+            printf("%s\n", std[i].name);
+    }
+}
+
+void prints_data(int roll, struct data *std, int n){
+    for(int i=0; i<=n; i++){
+        if(std[i].roll_number==roll)
+            printf("%d%s%s%s%d\n", std[i].roll_number, std[i].name, std[i].department, std[i].course, std[i].yoj);
+    }
+}
