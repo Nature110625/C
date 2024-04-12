1e53ab99c509c7e779ad16e7a2e57cd2e6c0d906
Prakriti <prakriti11082003@gmail.com>
2024-04-12 22:05:29 +0530

    ...

--- LetUsC_ch17_Structures/q2.c
+++ LetUsC_ch17_Structures/q2.c
@@ -12,27 +12,37 @@ below Rs. 100.*/
 
 #include <stdio.h>
 #include <conio.h>
+
+//created globally to get accesed by all function without using pointers
 struct data{
     int acc_no;
     char name[50];
     int balance;
-} cstmr[200];
+} cstmr[200];   // cstmr is an array, of structure, of type data
 
-void prnt_lbal(int size);
-int chbal(int i);
-int iof_ano(int ano, int s);
+void prnt_lbal(int size);   //prints data of customer if balance is less than 100
+int chbal(int i);           //returns 1 if given index customer's balance is less than 100
+int iof_ano(int ano, int s);    //return index of given account number
 
 int main(){
     int i, anum, amt, code, j;
     char ans;
+
+    //Takes entry of customers details 
     printf("Enter details (Account number, Name and Balance) of Customer (Enter 'no' to stop entering): \n");
     for (i=0; ;i++){
         printf(">");
-        if(!scanf("%d%s%d", &cstmr[i].acc_no, &cstmr[i].name, &cstmr[i].balance))
+        if(!scanf("%d%s%d", &cstmr[i].acc_no, &cstmr[i].name, &cstmr[i].balance))       //if scanf fails to take data
             break;
     }
+
+    //no of customers data filled is tracked by i
     prnt_lbal(i);
+    
+    //flushes the data stored in stdin
     fflush(stdin);
+
+    //ask for withdrawl or deposit
     printf("\n\nDo you want to request for withdrawl or deposit?\nEnter 'y' or 'n': ");
     scanf("%c", &ans);
     if(ans=='y'|| ans=='Y'){
@@ -43,9 +53,12 @@ int main(){
         scanf("%d", &amt);
         printf("Enter code (1 for deposit and 0 for withdrawl): ");
         scanf("%d", &code);
-        j=iof_ano(anum, i);
-        if(code==0 && chbal(j))
+
+        j=iof_ano(anum, i);     //stores index of account number entered by user in form
+        if(code==0 && chbal(j))     //if customer wants to withdraw and balance is less than 100
             printf("\nThe balance is insufficient for the specified withdrawal");
+        else
+            printf("\nRequest Successful");
     }
 }
 
